bf57df2dea6ac873c934ec7f83652607cece12e1
marchanti <martin.stefanec@edu.uni-graz.at>
2019-09-07 17:19:32 +0200

    the script

---
+++ sound_level_analyzer.py
@@ -0,0 +1,66 @@
+# Load the required libraries:
+from scipy.io import wavfile
+from matplotlib import pyplot as plt
+import numpy as np
+import wave
+
+def _wav2array(nchannels, sampwidth, data):
+    """data must be the string containing the bytes from the wav file."""
+    num_samples, remainder = divmod(len(data), sampwidth * nchannels)
+    if remainder > 0:
+        raise ValueError('The length of data is not a multiple of '
+                         'sampwidth * num_channels.')
+    if sampwidth > 4:
+        raise ValueError("sampwidth must not be greater than 4.")
+
+    if sampwidth == 3:
+        a = np.empty((num_samples, nchannels, 4), dtype=np.uint8)
+        raw_bytes = np.fromstring(data, dtype=np.uint8)
+        a[:, :, :sampwidth] = raw_bytes.reshape(-1, nchannels, sampwidth)
+        a[:, :, sampwidth:] = (a[:, :, sampwidth - 1:sampwidth] >> 7) * 255
+        result = a.view('<i4').reshape(a.shape[:-1])
+    else:
+        # 8 bit samples are stored as unsigned ints; others as signed ints.
+        dt_char = 'u' if sampwidth == 1 else 'i'
+        a = np.fromstring(data, dtype='<%s%d' % (dt_char, sampwidth))
+        result = a.reshape(-1, nchannels)
+    return result
+
+def readwav(file):
+    """
+    Read a wav file.
+    Returns the frame rate, sample width (in bytes) and a numpy array
+    containing the data.
+    This function does not read compressed wav files.
+    """
+    wav = wave.open(file)
+    rate = wav.getframerate()
+    nchannels = wav.getnchannels()
+    sampwidth = wav.getsampwidth()
+    nframes = wav.getnframes()
+    data = wav.readframes(nframes)
+    wav.close()
+    array = _wav2array(nchannels, sampwidth, data)
+    return rate, sampwidth, array
+
+# Load the data and calculate the time of each sample
+samplerate, width, data = readwav('HO_0002S1.wav')
+
+print(samplerate)
+data = np.absolute(data)
+data = data[1::(samplerate*60)]
+times = np.arange(len(data))
+print(len(times))
+
+# Make the plot
+# You can tweak the figsize (width, height) in inches
+plt.figure(figsize=(30, 4))
+#plt.fill_between(times, data[:,0], data[:,1], color='k') 
+plt.plot(times, data)
+plt.xlim(times[0], times[-1])
+plt.xlabel('time (s)')
+plt.ylabel('amplitude')
+# You can set the format by changing the extension
+# like .pdf, .svg, .eps
+plt.savefig('plot.png', dpi=100)
+plt.show()
